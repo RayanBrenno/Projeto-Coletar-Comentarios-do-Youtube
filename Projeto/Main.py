48c5f6b8d0b1bdce846239fafcd5a6bd52346cd0
RayanBrenno <rayanbrennodev@gmail.com>
2025-06-30 15:20:58 -0300

    ongoing

--- Projeto/Main.py
+++ Projeto/Main.py
@@ -1,8 +1,3 @@
 from script_front import open_login_window
-from script_banco_de_dados import create_all_tables
-from script_ia_teste import treinar_modelos
 
-
-treinar_modelos()
-create_all_tables()
 open_login_window()
