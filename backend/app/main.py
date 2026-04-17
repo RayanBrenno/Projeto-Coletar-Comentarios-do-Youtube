d49c88fa0078cad6b4f3835fe43e9989420b7ab7
Rayan Brenno <rayanbrennodev@gmail.com>
2026-04-17 12:08:45 -0300

    feat: add initial web structure with login and register pages

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes.analises import router as analises_router
+from app.routes.autenticacao import router as auth_router
+
+app = FastAPI(
+    title="API de Análise de Crédito",
+    version="1.0.0"
+)
+
+API_FRONT_URL = os.getenv("API_FRONT_URL")
+# API_FRONT_URL = "http://localhost:5173"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[API_FRONT_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(analises_router)
+app.include_router(auth_router)
