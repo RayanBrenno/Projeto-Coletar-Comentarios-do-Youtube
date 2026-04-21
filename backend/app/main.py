6b50418e9318f42bc2d390c8691ee1d75b1d65bb
Rayan Brenno <rayanbrennodev@gmail.com>
2026-04-21 10:57:13 -0300

    refactor: fix incorrect import paths by adding missing utils module in FastAPI project structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,10 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes.analises import router as analises_router
 from app.routes.autenticacao import router as auth_router
 
 app = FastAPI(
-    title="API de Análise de Crédito",
+    title="Projeto Coletar Comentários do YouTube",
     version="1.0.0"
 )
 
@@ -20,5 +19,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(analises_router)
 app.include_router(auth_router)
