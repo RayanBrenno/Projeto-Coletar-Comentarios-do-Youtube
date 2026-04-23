441c84c8bda235050d990ddd1c33266bb2a6bba2
Rayan Brenno <rayanbrennodev@gmail.com>
2026-04-23 12:12:37 -0300

    refactor: simplify api service

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,15 +1,7 @@
 import axios from "axios";
 
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
+const api = axios.create({
+  baseURL: "http://localhost:8000",
 });
 
-api.interceptors.request.use((config) => {
-  const token = sessionStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
+export default api;
